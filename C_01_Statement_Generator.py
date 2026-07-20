365b0b1cb8ce1d8e9bde866cf847d3cb8b20d5e5
JayVanilla <leaotuitamaj50371@masseyhigh.school.nz>
2026-07-20 13:11:01 +1200

    Working statement generator

--- C_01_Statement_Generator.py
+++ C_01_Statement_Generator.py
@@ -1,27 +1,8 @@
-# Generates headings (eg: ---- Heading ----)
+# functions go here
 
 def statement_generator(statement, decoration):
     print(f"\n{decoration * 5} {statement} {decoration * 5}")
 
-#Displays instructions
-def instructions():
-    statement_generator("instructions", "-")
-
-    print('''
-Instructions go here.
-- instruction 1
-- instruction 2
-- etc
-    ''')
-
-
 # Main routine goes here
+statement_generator("Instructions", "-")
 
-# Display instructions if requested
-want_instructions = input("Press <enter> to read the instructions" " or any key to continue")
-
-
-if want_instructions == "":
-    instructions()
-
-print("program continues")
